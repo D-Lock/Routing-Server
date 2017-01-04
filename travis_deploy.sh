75ff64a25eb59aa8ab2b6f3b25df5e76c7a1dd2b
RedbackThomson <redbackthomson@gmail.com>
2017-01-05 00:03:01 +0800

    Moved Travis deploy script to access build config

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -6,7 +6,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Deploying $TRAVIS_BRANCH $TRAVIS_COMMIT"
     
-    docker-compose --file ../docker-compose.build.yml up --force-recreate
+    docker-compose --file docker-compose.build.yml up --force-recreate
   else
     echo "Skipping deploy because it's not an allowed branch"
   fi
